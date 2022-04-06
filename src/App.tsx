0217bcc14e946e2702e79261d75fc86980bb3c0b
Emerson Silva <emersonsilva@MacBook-Pro-de-Tiba.local>
2022-04-05 22:24:10 -0300

    config project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Emerson</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
