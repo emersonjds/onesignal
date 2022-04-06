5e584224d430cd17bdc4d62d0e8f0d8d618bab39
Emerson Silva <emerson_jdss@hotmail.com>
2022-04-06 15:07:59 -0300

    adjust configs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+    return (
+        <View>
+            <Text>
+                Emerson
+            </Text>
+        </View>
+    )
+}
+
+export default App;
