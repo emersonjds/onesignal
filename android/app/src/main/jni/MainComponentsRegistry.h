308481db565869790015f9b9e4379c3aa3fb8550
Emerson Silva <emerson_jdss@hotmail.com>
2022-04-06 11:18:36 -0300

    adjusts on project

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/onesignal/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/myapp/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
--- ios/MyApp/AppDelegate.mm
+++ ios/MyApp/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"onesignal", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"MyApp", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
