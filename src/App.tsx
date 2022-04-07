2495ec893eb17aca349ad503050cdf0d5ade7527
Emerson Silva <emerson_jdss@hotmail.com>
2022-04-06 22:20:40 -0300

    adjust screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 
 const App: React.FC = () => {
-    return (
-        <SafeAreaView>
-            <View style={{
-                padding: 20
-            }}>
-                <Text>
-                    Emerson
-                </Text>
-            </View>
-        </SafeAreaView>
+  return (
+    <SafeAreaView>
+      <View style={{
+        padding: 20
+      }}>
+        <Text>
+          Emerson
+        </Text>
+      </View>
+    </SafeAreaView>
 
-    )
+  )
 }
 
 export default App;
