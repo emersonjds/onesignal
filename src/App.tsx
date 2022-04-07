c5f6618269e8d6e531a58c97cee1f1f99c066ade
Emerson Silva <emerson_jdss@hotmail.com>
2022-04-07 13:52:21 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 
 const App: React.FC = () => {
-    return (
-        <SafeAreaView>
-            <View style={{
-                padding: 20
-            }}>
-                <Text>
-                    Emerson
-                </Text>
-            </View>
-        </SafeAreaView>
-
-    )
-}
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+      }}>
+      <View
+        style={{
+          flex: 1,
+          padding: 20,
+          backgroundColor: '#fff',
+        }}>
+        <Text
+          style={{
+            color: '#000',
+          }}>
+          One Signal
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default App;
